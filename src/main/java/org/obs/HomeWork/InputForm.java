fe2e9393dc049b38c03f2134e56c97e2e9cf7fbd
AnasNazar880 <102611445+AnasNazar880@users.noreply.github.com>
2023-08-01 11:21:58 +0530

    commit3

--- src/main/java/org/obs/HomeWork/InputForm.java
+++ src/main/java/org/obs/HomeWork/InputForm.java
@@ -1,32 +1,27 @@
 package org.obs.HomeWork;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class InputForm {
     public static void main(String[] args) {
         WebDriver driver;
-        driver=new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.get("https://selenium.obsqurazone.com/simple-form-demo.php");
-        String actualTitle=driver.getTitle();
+        String actualTitle = driver.getTitle();
         System.out.println(actualTitle);
-        WebElement enterMessageField=driver.findElement(By.id("single-input-field"));
+        WebElement enterMessageField = driver.findElement(By.id("single-input-field"));
         enterMessageField.sendKeys("Welcome to obsquira");
-        WebElement showMessageButton=driver.findElement(By.xpath("//*[@id=\"button-one\"]"));
+        WebElement showMessageButton = driver.findElement(By.xpath("//*[@id=\"button-one\"]"));
         showMessageButton.click();
-        WebElement enterValueAField=driver.findElement(By.cssSelector("#value-a"));
+        WebElement enterValueAField = driver.findElement(By.cssSelector("#value-a"));
         enterValueAField.sendKeys("6");
-        WebElement enterValueBField= driver.findElement(By.cssSelector("#value-b"));
+        WebElement enterValueBField = driver.findElement(By.cssSelector("#value-b"));
         enterValueBField.sendKeys("9");
-        WebElement getTotalButton=driver.findElement(By.cssSelector("#button-two"));
+        WebElement getTotalButton = driver.findElement(By.cssSelector("#button-two"));
         getTotalButton.click();
 
-
-
-        
     }
 }

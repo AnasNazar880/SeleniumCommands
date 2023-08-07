b631beefc77464cd62f0f3eb4b2c4442b6f2ce64
AnasNazar880 <102611445+AnasNazar880@users.noreply.github.com>
2023-08-07 21:52:35 +0530

    coomit 5

--- src/main/java/org/obs/basics/DemoWebPage.java
+++ src/main/java/org/obs/basics/DemoWebPage.java
@@ -1,10 +1,8 @@
 package org.obs.basics;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class DemoWebPage {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();

dbe06a773d5bfed3e72e3776c5be80a826db8a69
AnasNazar880 <102611445+AnasNazar880@users.noreply.github.com>
2023-08-07 21:51:30 +0530

    coomit 5

--- src/main/java/org/obs/basics/DemoWebPage.java
+++ src/main/java/org/obs/basics/DemoWebPage.java
@@ -7,28 +7,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoWebPage {
     public static void main(String[] args) {
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.get("https://demowebshop.tricentis.com/login");
-        WebElement register=driver.findElement(By.xpath("//a[text()='Register']"));
+        WebElement register = driver.findElement(By.xpath("//a[text()='Register']"));
         register.click();
-        WebElement gender=driver.findElement(By.xpath("//input[@id='gender-male' and @name='Gender']"));
+        WebElement gender = driver.findElement(By.xpath("//input[@id='gender-male' and @name='Gender']"));
         gender.click();
-        WebElement firstName=driver.findElement(By.xpath("//input[@id='FirstName' and @name='FirstName']"));
+        WebElement firstName = driver.findElement(By.xpath("//input[@id='FirstName' and @name='FirstName']"));
         firstName.sendKeys("Anas");
-        WebElement lastName=driver.findElement(By.xpath("//input[@id='LastName' and @name='LastName']"));
+        WebElement lastName = driver.findElement(By.xpath("//input[@id='LastName' and @name='LastName']"));
         lastName.sendKeys("Nazar");
-        WebElement email=driver.findElement(By.xpath("//input[@class='text-box single-line' and @id='Email']"));
+        WebElement email = driver.findElement(By.xpath("//input[@class='text-box single-line' and @id='Email']"));
         email.sendKeys("anas1@gmail.com");
-        WebElement passWord=driver.findElement(By.xpath("//input[@id='Password' and @name='Password']"));
+        WebElement passWord = driver.findElement(By.xpath("//input[@id='Password' and @name='Password']"));
         passWord.sendKeys("Izin@2020");
-        WebElement confirmPass=driver.findElement(By.xpath("//input[@id='ConfirmPassword' and @name='ConfirmPassword']"));
+        WebElement confirmPass = driver.findElement(By.xpath("//input[@id='ConfirmPassword' and @name='ConfirmPassword']"));
         confirmPass.sendKeys("Izin@2020");
-        WebElement register1=driver.findElement(By.xpath("//input[@id='register-button' and @name='register-button' ]"));
+        WebElement register1 = driver.findElement(By.xpath("//input[@id='register-button' and @name='register-button' ]"));
         register1.click();
-
-
-
     }
 }

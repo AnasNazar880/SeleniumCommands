e22b2f9f910ff87bc72e8e52ea37920f262ebe0d
AnasNazar880 <102611445+AnasNazar880@users.noreply.github.com>
2023-08-09 09:09:43 +0530

    commit 6

--- src/main/java/org/obs/utility/RandomData.java
+++ src/main/java/org/obs/utility/RandomData.java
@@ -1,5 +1,7 @@
 package org.obs.utility;
 import com.github.javafaker.Faker;
+import com.github.javafaker.PhoneNumber;
+
 public class RandomData {
     public static Faker faker;
 
@@ -14,5 +16,33 @@ public class RandomData {
         String lastName = faker.name().lastName();
         return lastName;
     }
+    public static String getMobileNumber(){
+        faker=new Faker();
+      String mNumber=faker.phoneNumber().cellPhone();
+      return mNumber;
+    }
+    public static String getStreetName(){
+        faker=new Faker();
+        String streetName=faker.address().streetName();
+        return streetName;
+    }
+    public static String getCity(){
+        faker=new Faker();
+        String city=faker.address().country();
+        return city;
+    }
+    public static String getState(){
+        faker=new Faker();
+        String state=faker.address().state();
+        return state;
+    }
+    public static String getPinCode(){
+        faker=new Faker();
+        String PinCode=faker.address().zipCode();
+        return PinCode;
+    }
+
+
+
 }
 
